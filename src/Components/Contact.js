b04ac1cc1d41f62f2d5a061345f65ba97342e507
August Reid <c.august.reid@gmail.com>
2022-10-02 16:20:25 -0600

    Add Contact component

---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+
+function Contact() {
+  return(
+    <main>
+      <h2>Connect with me!</h2>
+      <p>LinkedIn</p>
+      <p>GitHub</p>
+      <p>c.august.reid@gmail.com</p>
+    </main>
+  )
+}
+
+export default Contact;
